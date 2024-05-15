8d1a5bcdc7034a5b1bc1a12eeb5c86c0304ab596
jadkw <jadkw6@gmail.com>
2024-05-15 13:15:53 -0400

    update file

--- app.js
+++ app.js
@@ -1,36 +1,44 @@
-let darkToggle = true
-function toggleContrast(){
+let darkToggle = false
+function toggleContrast() {
     darkToggle = !darkToggle;
-    if(darkToggle){
+    if (darkToggle) {
         document.body.classList += " dark-theme"
     }
-    else{
+    else {
         document.body.classList.remove("dark-theme")
     }
 }
 
 
-async function fetchData(){
-   
-   try{
-       const pokemonName = document.getElementById("pokemonName").value
-       const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
- 
-       if(!response.ok){
-        throw new Error("Could not fetch resource");
-       }
+const pokemonListEl = document.querySelector(`.pokemon__wrapper`)
 
-       const data = await response.json();
-       const pokemonSprite = data.sprites.front_default;
-       const imgElement = document.getElementById("pokemonSprite");
+async function fetchData() {
 
-       imgElement.src = pokemonSprite;
-       imgElement.style.display = "block";
+    try {
+        const pokemonName = document.getElementById("pokemonName").value
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+        const data = await response.json();
+
+        
+        const pokemonSprite = data.sprites.front_default;
+        const imgElement = document.getElementById("pokemonSprite");
+        
+        imgElement.src = pokemonSprite;
+        imgElement.style.display = "";
+        
+        console.log(data)
+        
+        if (!response.ok) {
+            throw new Error("Could not catch Pokemon");
+        }
     }
 
-    catch(error){
-    console.error(error)
-    } 
-    
+    catch (error) {
+        console.error(error)
+    }
+
 }
+
+
+
 
